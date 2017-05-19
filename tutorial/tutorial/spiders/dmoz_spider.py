2fd6915fc6e730e915694c03daceac670bec4624
Argon-H <neonstk@163.com>
2017-05-19 18:03:48 +0800

    修改爬虫

--- tutorial/tutorial/spiders/dmoz_spider.py
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -1,13 +1,34 @@
-import scrapy
+import os
+import urllib
+
+import scrapy,re
+pictureSrc = "D://pythonProject//new2"
 class DemoSprider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = ["dmoz.org"]
     start_urls = [
-        "https://my.oschina.net/xueba/blog/492948"
+        "https://www.pixiv.net/"
     ]
 
     def parse(self, response):
         # filename = response.url.split("/")[-2]
         # with open(filename,"wb") as  f:
         #     f.write(response.body)
-        print("66666"+str(response.body))
+        for link, t in set(re.findall(r'([a-zA-z]+://[^\s]*(jpg|png|gif))', str(response.body))):
+
+            print(link)
+            try:
+                urllib.request.urlretrieve(link, saveFlie(link))
+            except:
+                print('失败')
+        # print("66666"+str(response.body))
+
+
+
+def saveFlie(path):
+    if not os.path.isdir(pictureSrc):
+        os.mkdir(pictureSrc)
+    pos = path.rindex('/')
+    # print(path)
+    t = os.path.join(pictureSrc, path[pos + 1:])
+    return t
